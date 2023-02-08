f77450a89a302366dca8e40f9f30fe20848f9cc8
walidlec <walid.lec@gmail.com>
2023-02-08 02:20:51 +0100

    update fichiers audios et categories

--- irys-noises-app/src/App.vue
+++ irys-noises-app/src/App.vue
@@ -2,6 +2,14 @@
 import HelloWorld from './components/HelloWorld.vue';
 import IRySNoise from './components/IRySNoise.vue';
 
+
+import CollabRyS from './assets/AudioLists/CollabRyS.json';
+import General from './assets/AudioLists/General.json';
+import IRySJP from './assets/AudioLists/IRySJP.json';
+import IRySnoises from './assets/AudioLists/IRySnoises.json';
+import IRySoFunny from './assets/AudioLists/IRySoFunny.json';
+
+import WorldWideRyS from './assets/AudioLists/WorldWideRyS.json';
 import IRySneeze from './assets/AudioLists/IRySneeze.json';
 import IRyScream from './assets/AudioLists/IRyScream.json';
 import IRySoCute from './assets/AudioLists/IRySoCute.json';
@@ -21,6 +29,30 @@ import IRySinging from './assets/AudioLists/IRySinging.json';
 
   
   <h1> IRyS Noises!</h1>
+  <div >
+    <h2> CollabRyS</h2>
+    <IRySNoise v-for="item in CollabRyS" :category="item.category" :file="item.file" :title="item.title" class/>
+  </div>
+  <div >
+    <h2> General</h2>
+    <IRySNoise v-for="item in General" :category="item.category" :file="item.file" :title="item.title" class/>
+  </div>
+  <div >
+    <h2> IRySJP</h2>
+    <IRySNoise v-for="item in IRySJP" :category="item.category" :file="item.file" :title="item.title" class/>
+  </div>
+  <div >
+    <h2> IRySnoises</h2>
+    <IRySNoise v-for="item in IRySnoises" :category="item.category" :file="item.file" :title="item.title" class/>
+  </div>
+  <div >
+    <h2> IRySoFunny</h2>
+    <IRySNoise v-for="item in IRySoFunny" :category="item.category" :file="item.file" :title="item.title" class/>
+  </div>
+  <div >
+    <h2> WorldWideRyS</h2>
+    <IRySNoise v-for="item in WorldWideRyS" :category="item.category" :file="item.file" :title="item.title" class/>
+  </div>
   <div >
     <h2> IRySneeze</h2>
     <IRySNoise v-for="item in IRySneeze" :category="item.category" :file="item.file" :title="item.title" class/>
