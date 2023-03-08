607df5493a3fb3f3aff712a16f66bb19f70a6e2d
Walt280 <32001362+Walt280@users.noreply.github.com>
2023-03-07 20:56:10 -0500

    Random button now shows which clip is being played. CSS re-organization redux please dont break the mobile layout

--- src/components/IRySRandomNoise.vue
+++ src/components/IRySRandomNoise.vue
@@ -5,13 +5,16 @@
     soundObjs: Array
   });
 
+  const idleMsg = "🎲 Click/Tap here for a random sound! 🎲";
   const isPlayed = ref(false);
+  const buttonText = ref(idleMsg);
+
   let timeout;
   let played;
   let duration;
-  console.log(soundObjs);
   let soundList = soundObjs.flat();
-  console.log(soundList);
+
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
@@ -23,6 +26,7 @@
       played.pause()
       played.currentTime = 0;
       isPlayed.value = false;
+      buttonText.value = idleMsg;
     }else{
       let randomInt = getRandomInt(soundList.length);
       let randomSound = soundList[randomInt];
@@ -31,12 +35,17 @@
       played.play()
 
       played.addEventListener("loadeddata", () => {
-        duration = played.duration
+        duration = played.duration;
         isPlayed.value = true;
+        buttonText.value = `Now playing: ${randomSound.title}`;
         timeout = setTimeout(() =>{
-          isPlayed.value = false
+          isPlayed.value = false;
         }, (duration * 1000))
-      })
+      });
+
+      played.addEventListener("ended", () => {
+        buttonText.value = idleMsg;
+      });
     }
   }
 </script>
@@ -45,39 +54,17 @@
   <a>
     <button :class="{ played: isPlayed }" type="button" @click="playRandomSound();" :style="{'animation-duration': duration + 's'}">
         <div id="btn-overlay">
-            🎲 Click/Tap here for a random sound! 🎲
+            {{ buttonText }}
         </div>
     </button>
   </a>
 </template>
 
+<style src="../assets/css/button.css" scoped />
 <style scoped>
-
-
-.played {
-    background-image: url('../assets/img/btn-background.png');
-    background-size: 10% 100%;
-    background-repeat: repeat-y;
-    animation: linear;
-    animation-name: play;
-    border-color: #5e0136;
-  }
-
-  @keyframes play{
-    0%{
-      background-size: 0% 100% ;
-    }
-    100%{
-      background-size: 100% 100%;
+  @media screen and (max-width:  720px) {
+    button{
+      font-size: 4vw;
     }
   }
-  
-@media screen and (max-width:  720px) {
-  button{
-    font-size: 4vw;
-
-  }
-
-}
-
 </style>
